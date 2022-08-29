15bedf6f0c6471266612d5940a601ab73dc6ad58
Yair Peretz <yairperetz03@gmail.com>
2022-08-30 01:06:46 +0300

    Removed GUI project for now

--- HoradotTV/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace HoradotTV;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
