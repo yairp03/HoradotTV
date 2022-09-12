190cfd65a92c8cc7b305ecf83b867739035c993e
Yair Peretz <yairperetz03@gmail.com>
2022-09-13 01:43:26 +0300

    Added null checks

---
+++ SdarotAPI/Exceptions/WebsiteErrors/ElementNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace SdarotAPI.Exceptions.WebsiteErrors;
+
+public class ElementNotFoundException : WebsiteErrorException
+{
+    public ElementNotFoundException()
+    {
+    }
+
+    public ElementNotFoundException(string? element) : base($"The {element} element was not found. Please contact developer.")
+    {
+    }
+
+    public ElementNotFoundException(string? message, Exception? inner) : base(message, inner)
+    {
+    }
+}
