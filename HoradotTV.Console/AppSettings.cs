ba905ed368bff797427647ef641e1666004532fb
Yair Peretz <yairperetz03@gmail.com>
2022-09-28 23:03:20 +0300

    Saving last download location

---
+++ HoradotTV.Console/AppSettings.cs
@@ -0,0 +1,29 @@
+﻿namespace HoradotTV.Console;
+
+public class AppSettings
+{
+    [JsonIgnore]
+    public static AppSettings Default { get; } = LoadSettings();
+    private const string filePath = "appsettings.json";
+
+    public string? LastPath { get; set; }
+
+    private static AppSettings LoadSettings()
+    {
+        var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, filePath);
+        try
+        {
+            return JsonSerializer.Deserialize<AppSettings>(File.ReadAllText(path)) ?? new();
+        }
+        catch (FileNotFoundException)
+        {
+            return new();
+        }
+    }
+
+    public void Save()
+    {
+        var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, filePath);
+        File.WriteAllText(path, JsonSerializer.Serialize(this, options: new JsonSerializerOptions() { WriteIndented = true }));
+    }
+}
