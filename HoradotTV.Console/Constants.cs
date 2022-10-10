f2921b03fdc08e9c617a3dc3155ebed23b79451e
Yair Peretz <yairperetz03@gmail.com>
2022-10-10 21:37:12 +0300

    Made the constants constants

--- HoradotTV.Console/Constants.cs
+++ HoradotTV.Console/Constants.cs
@@ -2,19 +2,19 @@
 
 internal static class Constants
 {
-    public static string SOFTWARE_VERSION = "1.3.0";
+    public const string SOFTWARE_VERSION = "1.3.0";
 
-    public static int QUERY_MIN_LENGTH = 2;
+    public const int QUERY_MIN_LENGTH = 2;
 
-    public static string DEFAULT_DOWNLOAD_LOCATION = KnownFolders.Downloads.Path;
+    public static readonly string DEFAULT_DOWNLOAD_LOCATION = KnownFolders.Downloads.Path;
 
-    public static string ChromeDownloadProblemGuide = "https://github.com/yairp03/HoradotTV/wiki/Chrome-download-problem";
-    public static string SdarotTVConnectionProblemGuide = "https://github.com/yairp03/HoradotTV/wiki/SdarotTV-connection-problem";
+    public const string ChromeDownloadProblemGuide = "https://github.com/yairp03/HoradotTV/wiki/Chrome-download-problem";
+    public const string SdarotTVConnectionProblemGuide = "https://github.com/yairp03/HoradotTV/wiki/SdarotTV-connection-problem";
 }
 
 internal static class Menus
 {
-    public static string MODES_MENU = "-- Download Modes --\n" +
+    public const string MODES_MENU = "-- Download Modes --\n" +
                                       "[0] Back to start\n" +
                                       "[1] Download Episode\n" +
                                       "[2] Download Episodes\n" +
